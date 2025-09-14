d8cb92c926cced50c20d9e72647b2a85e51a0a9d
MeDhJe <jeswinsuresh2005@gmail.com>
2025-09-15 04:27:25 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/proj');
+const uri = 'mongodb+srv://Jeswin:abc@m0.nq6iznc.mongodb.net/proj?retryWrites=true&w=majority';
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
-  console.log('Connected to MongoDB!');
+  console.log('Connected to MongoDB Atlas!');
 });
 
 module.exports = db;
